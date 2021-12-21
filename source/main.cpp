5bbd7e31f042c1ba0a7733f1ecd2a72017e7cf75
ZachyCatGames <zach.kirtz@gmail.com>
2021-12-21 14:07:04 -0600

    Fix inconsistent indentation

--- source/main.cpp
+++ source/main.cpp
@@ -112,7 +112,7 @@ int main(int argc, char* argv[]) {
 }
 
 void PrintMaxClocks(const bool isMariko) {
-	ClkrstSession clkrstSession;
+    ClkrstSession clkrstSession;
     RgltrSession rgltrSession;
     u32 curFreq;
     u32 cpuFreq;
@@ -124,43 +124,43 @@ void PrintMaxClocks(const bool isMariko) {
 
     /* Get max clocks and voltages at those clocks for CPU, GPU, and RAM. */
     rgltrOpenSession(&rgltrSession, isMariko ? PcvPowerDomainId_Max77812_Cpu : PcvPowerDomainId_Max77621_Cpu);
-	clkrstOpenSession(&clkrstSession, PcvModuleId_CpuBus, 3);
-	clkrstGetClockRate(&clkrstSession, &curFreq);
-	clkrstSetClockRate(&clkrstSession, 4000000000);
+    clkrstOpenSession(&clkrstSession, PcvModuleId_CpuBus, 3);
+    clkrstGetClockRate(&clkrstSession, &curFreq);
+    clkrstSetClockRate(&clkrstSession, 4000000000);
     svcSleepThread(10000000);
     rgltrGetVoltage(&rgltrSession, &cpuVolt);
-	clkrstGetClockRate(&clkrstSession, &cpuFreq);
-	clkrstSetClockRate(&clkrstSession, curFreq);
-	clkrstCloseSession(&clkrstSession);
+    clkrstGetClockRate(&clkrstSession, &cpuFreq);
+    clkrstSetClockRate(&clkrstSession, curFreq);
+    clkrstCloseSession(&clkrstSession);
     rgltrCloseSession(&rgltrSession);
 
     rgltrOpenSession(&rgltrSession, isMariko ? PcvPowerDomainId_Max77812_Gpu : PcvPowerDomainId_Max77621_Gpu);
-	clkrstOpenSession(&clkrstSession, PcvModuleId_GPU, 3);
-	clkrstGetClockRate(&clkrstSession, &curFreq);
-	clkrstSetClockRate(&clkrstSession, 4000000000);
+    clkrstOpenSession(&clkrstSession, PcvModuleId_GPU, 3);
+    clkrstGetClockRate(&clkrstSession, &curFreq);
+    clkrstSetClockRate(&clkrstSession, 4000000000);
     svcSleepThread(10000000);
     rgltrGetVoltage(&rgltrSession, &gpuVolt);
-	clkrstGetClockRate(&clkrstSession, &gpuFreq);
-	clkrstSetClockRate(&clkrstSession, curFreq);
-	clkrstCloseSession(&clkrstSession);
+    clkrstGetClockRate(&clkrstSession, &gpuFreq);
+    clkrstSetClockRate(&clkrstSession, curFreq);
+    clkrstCloseSession(&clkrstSession);
     rgltrCloseSession(&rgltrSession);
 
     rgltrOpenSession(&rgltrSession, isMariko ? PcvPowerDomainId_Max77812_Dram : PcvPowerDomainId_Max77620_Sd1);
-	clkrstOpenSession(&clkrstSession, PcvModuleId_EMC, 3);
-	clkrstGetClockRate(&clkrstSession, &curFreq);
-	clkrstSetClockRate(&clkrstSession, 4000000000);
+    clkrstOpenSession(&clkrstSession, PcvModuleId_EMC, 3);
+    clkrstGetClockRate(&clkrstSession, &curFreq);
+    clkrstSetClockRate(&clkrstSession, 4000000000);
     svcSleepThread(10000000);
     rgltrGetVoltage(&rgltrSession, &emcVolt);
-	clkrstGetClockRate(&clkrstSession, &emcFreq);
-	clkrstSetClockRate(&clkrstSession, curFreq);
-	clkrstCloseSession(&clkrstSession);
+    clkrstGetClockRate(&clkrstSession, &emcFreq);
+    clkrstSetClockRate(&clkrstSession, curFreq);
+    clkrstCloseSession(&clkrstSession);
     rgltrCloseSession(&rgltrSession);
 
     /* Print the maximum clocks and voltages obtained earlier */
-	printf("Maximum Usable Clocks:\n");
-	printf("CPU Clock: %6.1fMHz\n", (double)cpuFreq / 1000000);
+    printf("Maximum Usable Clocks:\n");
+    printf("CPU Clock: %6.1fMHz\n", (double)cpuFreq / 1000000);
     printf("GPU Clock: %6.1fMHz\n", (double)gpuFreq / 1000000);
-	printf("EMC Clock: %6.1fMHz\n", (double)emcFreq / 1000000);
+    printf("EMC Clock: %6.1fMHz\n", (double)emcFreq / 1000000);
 
     printf("\nVoltage:\n");
     printf("CPU Voltage: %6.1fmV\n", (double)cpuVolt / 1000);
@@ -169,7 +169,7 @@ void PrintMaxClocks(const bool isMariko) {
 }
 
 void PrintAllClocks(const bool isMariko) {
-	ClkrstSession clkrstSession;
+    ClkrstSession clkrstSession;
     RgltrSession rgltrSession;
     u32 curFreq;
     u32 freq;
@@ -177,7 +177,7 @@ void PrintAllClocks(const bool isMariko) {
 
     /* Attempt to set CPU/GPU to all clocks in the lists above and get voltages. */
     rgltrOpenSession(&rgltrSession, isMariko ? PcvPowerDomainId_Max77812_Cpu : PcvPowerDomainId_Max77621_Cpu);
-	clkrstOpenSession(&clkrstSession, PcvModuleId_CpuBus, 3);
+    clkrstOpenSession(&clkrstSession, PcvModuleId_CpuBus, 3);
     clkrstGetClockRate(&clkrstSession, &curFreq);
     printf("CPU: \n");
     for(unsigned int i = 0; i < sizeof(g_CpuClockList)/sizeof(u32); i++) {
@@ -194,7 +194,7 @@ void PrintAllClocks(const bool isMariko) {
     rgltrCloseSession(&rgltrSession);
 
     rgltrOpenSession(&rgltrSession, isMariko ? PcvPowerDomainId_Max77812_Gpu : PcvPowerDomainId_Max77621_Gpu);
-	clkrstOpenSession(&clkrstSession, PcvModuleId_GPU, 3);
+    clkrstOpenSession(&clkrstSession, PcvModuleId_GPU, 3);
     clkrstGetClockRate(&clkrstSession, &curFreq);
     printf("\nGPU: \n");
     for(unsigned int i = 0; i < sizeof(g_GpuClockList)/sizeof(u32); i++) {
